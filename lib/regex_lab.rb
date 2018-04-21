08d82f32b920f9756dac7e8d0fcd7c979064b8b4
Scott Silverman <scottms724@gmail.com>
2018-04-21 15:12:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[aeiou]/)
+  if word.match(/\b[aeiou]/)
     return true 
-  elsif word.scan(/\b[bcdfghjklmnpqrstvqxyz]/)
+  elsif word.match(/\b[bcdfghjklmnpqrstvqxyz]/)
     return false
   end
 end
